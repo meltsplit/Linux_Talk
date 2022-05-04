ada8ad82a2fa5643de057092da372e4c23f8d51c
Snshadow <randy0007@naver.com>
2022-05-04 13:18:45 +0900

    Menu selection

--- login.sh
+++ login.sh
@@ -1,9 +1,19 @@
-declare -i i
-declare -i count
-echo -ne "Number of line: "
-read count
+#! /bin/bash
 
-for (( i=0; i<$count; i++ ))
+declare -a arr
+declare -i num
+PS3="Input number(1-3): "
+
+echo "MAIN MENU"
+select ch in "sign in" "sign up" "exit"
 do
-	echo "-------------------------------------"
+case "${ch}" in
+"sign in") echo "sign in"
+break ;;
+"sign up") echo "sign up"
+break ;;
+"exit") echo "quit"
+break ;;
+*) echo "[31mInvalid[0m" ;;
+esac
 done
