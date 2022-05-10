b2b7fa36a6dcc87b43295f26973ddcd0979877b0
mingyu <thdalsrb79@naver.com>
2022-05-11 01:09:01 +0900

    chatroom err fix

--- chatroom_list.sh
+++ chatroom_list.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+username=$1
 
 chatroom_select() {
     clear
@@ -7,12 +8,14 @@ chatroom_select() {
     select chat_num in "room1" "room2" "room3" "room4"
     do
 	    case ${chat_num} in
-		    "room1") bash room1.sh;;
+		    "room1") bash room1.sh ${username};;
 		    "room2") echo "need script";;
 		    "room3") echo "need script";;
 		    "room4") echo "need script";;
 	    esac
+		break;
 		done
+		chatroom_select
 }
 
 chatroom_select
