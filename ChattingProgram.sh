e2f1ea718be4db70e60edc9a6d8a99bb3c301970
meltsplit <seokwoo2000@naver.com>
2022-05-11 03:22:09 +0900

    [대격변]뷰당 파일 하나씩으로 설정

---
+++ ChattingProgram.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+declare -a arr
+declare -i num
+export username="default name"
+
+
+
+mainView(){
+
+    opt=0
+	while [ $opt != 3 ]
+	do
+    echo "[ Main View ]"
+	echo "1) Sign In"
+	echo "2) Sign up"
+	echo "3) Exit"
+	while [ true ]
+	do
+		read -p "Choose menu(1-3): " opt
+		if [ ${opt} == 1 -o ${opt} == 2 -o ${opt} == 3 ]; then
+			break
+		fi
+	done
+	
+	case ${opt} in
+	"1") bash SignInView.sh ;; #화면 전환
+	"2") bash SignUpView.sh ;; #화면 전환
+	"3") ;;
+	*) errorMode break;;
+	esac
+
+	done
+}
+
+main(){
+    mainView
+    echo "<<Program End>>"
+}
+
+#code start point
+main
--- RoomListView.sh
+++ RoomListView.sh
@@ -2,21 +2,21 @@
 
 username=$1
 
-chatroom_select() {
+RoomListView() {
     clear
+
     PS3="choose a room for chatting: "
-    select chat_num in "room1" "room2" "room3" "room4"
+    select chat_num in "room1" "room2" "room3" "room4" "Exit"
     do
 	    case ${chat_num} in
-		    "room1") bash room1.sh ${username};;
+		    "room1") bash room1.sh ${username};; #화면 전환
 		    "room2") echo "need script";;
 		    "room3") echo "need script";;
 		    "room4") echo "need script";;
+			"Exit") break;;
 	    esac
-		break;
-		done
-		chatroom_select
+	done
 }
 
-chatroom_select
+RoomListView
 
