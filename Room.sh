988068317ab7ca50aee2c4e1dfd5e6fbd1e9fbd2
meltsplit <seokwoo2000@naver.com>
2022-05-18 11:08:15 +0000

    [Feat] delete기능 구현

--- Room.sh
+++ Room.sh
@@ -37,37 +37,93 @@ sendMessage(){
 	echo "$(date);${username};${msg_s}" >> chatLog${opt_R}.txt
 		
 }
-deleteMessage(){	
+
+deleteMessageProgram(){
 	declare -i count=1
-    	clear
-    	echo " <<delete Message>> "
-	    while read line;
-		do
+	declare -i lineNum=1
+	while read line;
+	do
+
 		chatUser=`echo ${line}|cut -d ';' -f 2`
 		chatMessage=`echo ${line}|cut -d ';' -f 3`
-		
-		
+				
 		declare -i dateNow_s=`date '+%s'`
 		chatDate_full=`echo ${line}|cut -d ';' -f 1`
 		declare -i chatDate_s=`date -d "$chatDate_full" '+%s'`
 		declare -i timeInterval=`expr ${dateNow_s} - ${chatDate_s}`
+
 		if [ ${username} = ${chatUser} ]; then
 			if [ ${timeInterval} -le ${five_Minute} ]; then
-				echo "[${count}]"
-				echo "User : ${chatUser}" 
-				echo "Message : ${chatMessage}"
-				echo ""
-			
+				if [ ${count} = ${d_opt} ]; then
+					sed -i "${lineNum}s/${chatMessage}/---delete Message---/g" chatLog${opt_R}.txt	
+					
+				fi
 				count=`expr $count + 1`
-				sleep 1
 			fi
 		fi
 
+		lineNum=`expr $lineNum + 1`
+
 		done < chatLog${opt_R}.txt
+
+}
+
+deleteMessage(){	
+	while [ true ]
+	do
+		declare -i count=1
+    		clear
+    		echo " <<delete Message>> "
+	  	while read line;
+		do
+			chatUser=`echo ${line}|cut -d ';' -f 2`
+			chatMessage=`echo ${line}|cut -d ';' -f 3`
+		
+		
+			declare -i dateNow_s=`date '+%s'`
+			chatDate_full=`echo ${line}|cut -d ';' -f 1`
+			declare -i chatDate_s=`date -d "$chatDate_full" '+%s'`
+			declare -i timeInterval=`expr ${dateNow_s} - ${chatDate_s}`
+			if [ "${username}" = "${chatUser}" ]; then
+				if [ ${timeInterval} -le ${five_Minute} ]; then
+					echo "[${count}]"
+					echo "User : ${chatUser}" 
+					echo "Message : ${chatMessage}"
+					echo ""
+			
+					count=`expr $count + 1`
+				fi
+			fi
+		done < chatLog${opt_R}.txt
+		
+		echo "Select Number of Message which want to delete"
+		
+		d_opt=0
+		while [ "${d_opt}" != "q" ]
+		do
+			read -p "Input Number(quit= 'q'): " d_opt
+		
+	
+			if [ ${d_opt} -le ${count} -a ${d_opt} -ge 1 ]; then #IF INPUT 'Q' WARNING HERE	
+				deleteMessageProgram ${d_opt}
+				break;
+			fi
+		done
+		
+		if [ "${d_opt}" = "q" ]; then
+			break;
+		fi
+	done
+
+
 	echo "deleteMessage End"
-	sleep 5
+	sleep 2
 		
 }
+
+
+
+
 findMessage(){
     	clear
     	updateUI
@@ -127,6 +183,7 @@ roomView(){
 #code start point 
 
 roomView
+
 
 
 
