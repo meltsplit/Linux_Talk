f4dbcbbb10f018d35292d413f947a3e831b669ac
Yeom <76275691+prgmr99@users.noreply.github.com>
2022-05-27 13:43:54 +0900

    [Add] 다른 ip 추출 방법 추가

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# ip route get 8.8.8.8 | cut -d ' ' -f 7`로 하면 "wifi0" 같은 네트워크 인터페이스없이 ip만 뽑아올 수 있음.
 read -p "msg: " msg
 ip=`ifconfig | grep -A 1 -E "wifi0" | grep "inet" | tr -s ' ' | cut -d ' ' -f 3`
 echo ${msg} | nc -q 0 ${ip} 1234
