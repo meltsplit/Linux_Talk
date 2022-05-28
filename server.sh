91b6a2db57afa2081d448d5d1f7eb64c554853a1
Snshadow <randy0007@naver.com>
2022-05-28 16:20:36 +0900

    [Mod] 서버 스크립트에 명령 수행 구현(문제 해결 필요)

--- server.sh
+++ server.sh
@@ -1,26 +1,55 @@
 #! /bin/bash
 
+:<< EX
+nc를 통해 클라이언트 에게서 신호를 받으면 그 신호를 토대로 명령을 수행, 받은 신호는 inopt.txt에 추가하며 수행이 완료된 명령은 "executed"로 치환되어서 수행하였음을 표시
+입력을 한 번밖에 받지 않는 문제가 있음. 해결 시급
+EX
+
 ip=$1
 #not actual var
 port=$2
-switch=1
-rnum=1
+roomNum=1
+
+nc -lkd 1234 >> inopt.txt &
+nc -lkd 1400 >> inopt.txt &
+
+while true
+do
+	input=`tail -n 1 inopt.txt 2>/dev/null`
+	if [ "${input}" = "\"executed\"" -o -z "${input}" ];
+	then
+		echo "Idle state"
+	else
+		sed '$s/.*/"executed"/' -i inopt.txt
+		echo "${input}"
+	fi
+	sleep 0.4
+	echo $RANDOM
+done
 
 requestOp() {
 
-	ior=`nc -l 1234`
-	ip=`echo ${ior} | cut -d ' ' -f 1`
-	port=`echo ${ior} | cut -d ' ' -f 2`
-	opt=`echo ${ior} | cut -d ' ' -f 3`
-	pas=`echo ${ior} | cut -d ' ' -f 4`
-
+	ip=`echo ${input} | cut -d ' ' -f 1`
+	port=`echo ${input} | cut -d ' ' -f 2`
+	opt=`echo ${input} | cut -d ' ' -f 3`
+	pas=`echo ${input} | cut -d ' ' -f 4`
 
 }
 
-while [ "${switch}" = 1 ]
+while [ true ]
 do
-	if [ "${ior}" = 1 ];
+	requestOp
+	if [ "${opt}" = "Send" ];
 	then
-		${msg} >> chatLog${rnum}.txt
+		${msg} >> chatLog${roomNum}.txt
+	elif [ "${opt}" = "Delete" ];
+	then
+		export opt="Delete"
+	elif [ "${opt}" = "Exit" ]
+	then
+		break
+	else
+		nc -w4 -N ${ip} ${port} < chatLog${roomNum}.txt
 	fi
 done
+
