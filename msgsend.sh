38e74d086cfb7f2337660110878f42da6c80fd37
prgmr99 <johnyeom24@naver.com>
2022-05-29 22:58:37 +0900

    [Etc] how to tranfer IP and Port to server?

--- msgsend.sh
+++ msgsend.sh
@@ -3,9 +3,11 @@
 IP=$1
 PORT=$2
 
+# sending msg to server
+
 sendChat() {
-	timeout 1s nc 1234 #server IP is needed
-	timeout 1s nc 1234 < chatLog${roomNum}.txt #server IP is needed	
+	timeout 2s nc 1234 #server IP is needed
+	timeout 2s nc 1234 < chatLog${roomNum}.txt #server IP is needed	
 }
 
 sendChat
