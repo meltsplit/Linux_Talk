8339df192b96c94d5693a6b3f91ac9ddada0b3b3
prgmr99 <johnyeom24@naver.com>
2022-05-31 18:34:53 +0900

    [Mod] updated

--- Source/Server/msgsend.sh
+++ Source/Server/msgsend.sh
@@ -2,8 +2,8 @@ ip=$1
 port=$2
 
 sendChat() {
-	timeout 2s nc 10.20.8.75 1234 #server IP is needed
-	timeout 2s nc 10.20.8.75 1234 < ./Data/Chat/chatLog_${roomName}.txt #server IP is needed
+	timeout 2s nc 10.20.12.180 1234 
+	timeout 2s nc 10.20.12.180 1234 < ./Data/Chat/chatLog_${roomName}.txt 
 }
 
 sendChat
